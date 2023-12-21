e7fdd6ce844f319c7ca5c76a0b1a1431c2679eef
Chandidas Chatterjee <chandidaschatterjee327@gmail.com>
2023-12-21 14:46:13 +0530

    Change the data type

--- src/testnew/javanew.java
+++ src/testnew/javanew.java
@@ -4,8 +4,8 @@ public class javanew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	            double a = 5;    
-	            double b = 2;    
+	            int a = 53;    
+	            int b = 3;    
 	            System.out.println(Math.pow(a, b));
 	            System.out.println(a);
 	            System.out.println(b);
