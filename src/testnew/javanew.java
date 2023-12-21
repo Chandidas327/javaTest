678d701894baa08f6628b63a7786d7ef59346430
Chandidas Chatterjee <chandidaschatterjee327@gmail.com>
2023-12-21 13:13:28 +0530

    add new code

--- src/testnew/javanew.java
+++ src/testnew/javanew.java
@@ -4,7 +4,12 @@ public class javanew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	            double a = 5;    
+	            double b = 2;    
+	            System.out.println(Math.pow(a, b));  // return a^b i.e. 5^2  
+	        }    
+	       
 
 	}
 
-}
+
