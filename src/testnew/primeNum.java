90b4ba0b0526cdf84af98b4bdc42793a8fd8fe1b
Chandidas Chatterjee <chandidaschatterjee327@gmail.com>
2023-12-25 17:06:48 +0530

    new code on prime number

---
+++ src/testnew/primeNum.java
@@ -0,0 +1,25 @@
+package testnew;
+
+public class primeNum {
+	public static void main(String[] args) {
+		int num =100;
+		
+		for(int i = 2;i<num;i++)
+		{
+			int count = 0;
+			for (int y =2;y<i;y++)
+			{
+				if(i%y==0) {
+					count++;
+				}
+				
+				
+
+			
+		}
+			if (count==0) {
+				System.out.println(i);
+			}
+	}
+	}}
+
--- src/testnew/reverseNum.java
+++ src/testnew/reverseNum.java
@@ -5,12 +5,16 @@ public class reverseNum {
 	public static void main(String[] args) {
 		int n = 1267783;
 		int rev = 0;
+		int revNum = 0;
 		while(n>0)
 		{
 			rev = n%10;
-			System.out.print(rev);
-			n = n/10;
+			revNum = revNum * 10 + rev;
+		//	System.out.print(rev);
+					n = n/10;
 		}
+		System.out.println(revNum);
+		
 		
 	}
 
