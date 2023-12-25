fdc11bc50966a26ac45e007e7f2a5524d2b98180
Chandidas Chatterjee <chandidaschatterjee327@gmail.com>
2023-12-25 17:52:48 +0530

    swapTwonumWithoutThirdVariable

---
+++ src/testnew/swapTwonumWithoutThirdVariable.java
@@ -0,0 +1,17 @@
+package testnew;
+
+public class swapTwonumWithoutThirdVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=30;
+		b = a+b;
+		a=b-a;
+		System.out.println("a="+a);
+		b=b-a;
+		System.out.println("b="+b);
+
+	}
+
+}
